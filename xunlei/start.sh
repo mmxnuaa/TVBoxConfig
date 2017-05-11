854ac1be51eeb827a1c8b8965011ad3b33646dbc
zhiwei.meng <zhiwei.meng@jiagu-tech.com>
2017-05-11 19:36:49 +0800

    update xunlei config

--- xunlei/start.sh
+++ xunlei/start.sh
@@ -1,4 +1,4 @@
 #!/system/bin/sh
-cd /data/mmx/xunlei
-./lib/EmbedThunderManager 140730200100000350000007hmwm4v33j2e12s3etu > /dev/null &
+cd /data/mmx/xunlei/lib
+./EmbedThunderManager 140730200100000350000007hmwm4v33j2e12s3etu > /dev/null &
 
